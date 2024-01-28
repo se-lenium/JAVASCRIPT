0e6fb05f07b0ae543cd71f8db86d45a976c6bca0
se-lenium <155882969+se-lenium@users.noreply.github.com>
2024-01-28 07:59:50 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+let name = "Aditya"
+let age = 18
+let isLoggedIn = false
+
+//number; bigint
+//string = " "
+//boolean = true/false
+//null = standalone value ; representation of empty value
+//undefined = 
+//symbol = uniqueness of an element
+
+
+//object 
+
+console.log(typeof "Aditya") => string
+console.log(typeof null) => object
+console.log(typeof undefined) => undefined
---
+++ 01_basics/index.js
@@ -0,0 +1,6 @@
+const accId =  14453
+let accEmail = "aditya@gmail.com"
+var accPass = "12345"
+accCity = " Bhagalpur"
+
+console.table([ accId, accEmail, accPass, accCity])
